0dad23dca2f11ba0d8ecb27323ab731b0f3c33e7
Ricardo Delfin <me@rdelfin.com>
2020-08-24 23:47:56 +0100

    Added basic fork, exec, wait loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use nix::{
+    sys::wait::wait,
+    unistd::{execvp, fork, ForkResult},
+};
+use std::env;
+use std::ffi::{CStr, CString};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    match fork() {
+        Ok(ForkResult::Parent { child }) => {
+            println!("From the parent, child has PID {}", child);
+            wait().unwrap();
+            println!("Child finished!")
+        }
+        Ok(ForkResult::Child) => {
+            let args_cstr: Vec<_> = args
+                .iter()
+                .skip(1)
+                .map(|a| CString::new(a.as_bytes()).unwrap())
+                .collect();
+
+            let args_cstring: Vec<&CStr> = args_cstr.iter().map(|c| c.as_c_str()).collect();
+            println!(
+                "Running execvp on program \"{}\" with args {:?}",
+                args[0], args_cstring,
+            );
+            execvp(&CString::new(args[0].as_bytes()).unwrap(), &args_cstring).unwrap();
+        }
+        Err(_) => println!("Fork failed."),
+    }
 }
