96b01ed20a130d222223ffd5504132fb57bbe34f
Ricardo Delfin <me@rdelfin.com>
2020-09-02 22:57:43 +0100

    Moved lexer tokens into individual trait implementations

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -1,4 +1,4 @@
-use std::iter::Peekable;
+use std::{iter::Peekable, str::Chars};
 use thiserror::Error;
 
 use crate::parser::tokens::LexerToken;
@@ -14,27 +14,126 @@ pub enum LexingError {
 pub fn lexer_tokenise(code: &str) -> Result<Vec<LexerToken>, LexingError> {
     let mut tokens = vec![];
     let mut char_iter = code.chars().peekable();
+    // The order is important as it defines which matches are checked first
+    let parsers: Vec<Box<dyn Token>> = vec![
+        Box::new(TextParser),
+        Box::new(NewLineParser),
+        Box::new(WhitespaceParser),
+    ];
+
     loop {
         match char_iter.peek() {
             Some(c) => {
-                let next_token = if is_letter(*c) || is_number(*c) || is_valid_symbol(*c) {
-                    parse_text(&mut char_iter)
-                } else if is_new_line(*c) {
-                    parse_new_line(&mut char_iter)?
-                } else if c.is_whitespace() {
-                    parse_whitespace(&mut char_iter);
-                    continue;
-                } else {
-                    return Err(LexingError::InvalidCharError(*c));
-                };
+                let c = *c;
+                let mut found_match = false;
 
-                tokens.push(next_token);
+                for parser in parsers.iter() {
+                    if parser.matches(c) {
+                        found_match = true;
+                        let new_tokens = parser.parse(&mut char_iter)?;
+                        tokens.extend(new_tokens);
+                        break;
+                    }
+                }
+
+                if !found_match {
+                    return Err(LexingError::InvalidCharError(c));
+                };
             }
             None => break,
         }
     }
 
     Ok(tokens)
+}
+
+trait Token {
+    fn matches(&self, c: char) -> bool;
+    fn parse(&self, iter: &mut Peekable<Chars>) -> Result<Vec<LexerToken>, LexingError>;
+}
+
+struct TextParser;
+struct NewLineParser;
+struct WhitespaceParser;
+
+impl Token for TextParser {
+    fn matches(&self, c: char) -> bool {
+        is_letter(c) || is_number(c) || is_valid_symbol(c)
+    }
+
+    fn parse(&self, iter: &mut Peekable<Chars>) -> Result<Vec<LexerToken>, LexingError> {
+        let mut str_buf = String::new();
+
+        loop {
+            if let Some(c) = iter.peek() {
+                if is_letter(*c) || is_number(*c) || is_valid_symbol(*c) {
+                    str_buf.push(*c);
+                    iter.next();
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        Ok(vec![LexerToken::Text(str_buf)])
+    }
+}
+
+impl Token for NewLineParser {
+    fn matches(&self, c: char) -> bool {
+        c == '\r' || c == '\n'
+    }
+
+    fn parse(&self, iter: &mut Peekable<Chars>) -> Result<Vec<LexerToken>, LexingError> {
+        let c = iter.peek();
+
+        match c {
+            Some(&c) => {
+                if c == '\n' {
+                    iter.next();
+                } else if c == '\r' {
+                    if let Some(next_c) = iter.next() {
+                        if next_c != '\n' {
+                            return Err(LexingError::InvalidCharError(c));
+                        }
+                    } else {
+                        return Err(LexingError::UnexpectedEnd("new line".to_string()));
+                    }
+                } else {
+                    return Err(LexingError::InvalidCharError(c));
+                }
+            }
+            None => {
+                return Err(LexingError::UnexpectedEnd("new line".to_string()));
+            }
+        }
+
+        return Ok(vec![LexerToken::NewLine]);
+    }
+}
+
+impl Token for WhitespaceParser {
+    fn matches(&self, c: char) -> bool {
+        c.is_whitespace()
+    }
+
+    fn parse(&self, iter: &mut Peekable<Chars>) -> Result<Vec<LexerToken>, LexingError> {
+        loop {
+            if let Some(c) = iter.peek() {
+                if c.is_whitespace() {
+                    iter.next();
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        Ok(vec![])
+    }
 }
 
 fn is_letter(c: char) -> bool {
@@ -45,72 +144,6 @@ fn is_number(c: char) -> bool {
     c >= '0' && c <= '9'
 }
 
-fn is_new_line(c: char) -> bool {
-    c == '\r' || c == '\n'
-}
-
 fn is_valid_symbol(c: char) -> bool {
     ['-', '_', '.', '/'].contains(&c)
-}
-
-fn parse_text<T: Iterator<Item = char>>(iter: &mut Peekable<T>) -> LexerToken {
-    let mut str_buf = String::new();
-
-    loop {
-        if let Some(c) = iter.peek() {
-            if is_letter(*c) || is_number(*c) || is_valid_symbol(*c) {
-                str_buf.push(*c);
-                iter.next();
-            } else {
-                break;
-            }
-        } else {
-            break;
-        }
-    }
-
-    LexerToken::Text(str_buf)
-}
-
-fn parse_whitespace<T: Iterator<Item = char>>(iter: &mut Peekable<T>) {
-    loop {
-        if let Some(c) = iter.peek() {
-            if c.is_whitespace() {
-                iter.next();
-            } else {
-                return;
-            }
-        } else {
-            return;
-        }
-    }
-}
-
-fn parse_new_line<T: Iterator<Item = char>>(
-    iter: &mut Peekable<T>,
-) -> Result<LexerToken, LexingError> {
-    let c = iter.peek();
-
-    match c {
-        Some(&c) => {
-            if c == '\n' {
-                iter.next();
-            } else if c == '\r' {
-                if let Some(next_c) = iter.next() {
-                    if next_c != '\n' {
-                        return Err(LexingError::InvalidCharError(c));
-                    }
-                } else {
-                    return Err(LexingError::UnexpectedEnd("new line".to_string()));
-                }
-            } else {
-                return Err(LexingError::InvalidCharError(c));
-            }
-        }
-        None => {
-            return Err(LexingError::UnexpectedEnd("new line".to_string()));
-        }
-    }
-
-    return Ok(LexerToken::NewLine);
 }
